080659c803dcdb2d2f55cba04114e78d823ac307
Hamza elhoub <hamza.elhoub@gmail.com>
2022-03-16 10:13:02 +0100

    h2 table has been created

--- src/main/java/com/example/japap/JapApApplication.java
+++ src/main/java/com/example/japap/JapApApplication.java
@@ -1,13 +1,27 @@
 package com.example.japap;
 
+import com.example.japap.entities.Patient;
+import com.example.japap.repositories.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class JapApApplication {
+import java.util.Date;
 
+@SpringBootApplication
+public class JapApApplication implements CommandLineRunner {
+	@Autowired
+	private PatientRepository patientRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(JapApApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	patientRepository.save(new Patient(null,"hamza",new Date(),false,50));
+	patientRepository.save(new Patient(null,"yassir",new Date(),true,01));
+		patientRepository.save(new Patient(null,"issac",new Date(),false,90));
+
+	}
 }
--- src/main/java/com/example/japap/repositories/PatientRepository.java
+++ src/main/java/com/example/japap/repositories/PatientRepository.java
@@ -1,4 +1,6 @@
 package com.example.japap.repositories;
+import com.example.japap.entities.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PatientRepository extends JpaRepository<>{
+public interface PatientRepository extends JpaRepository<Patient, Long> {
 }
